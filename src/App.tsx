4ad614fd3a188a1827bda90068a44d195af052bf
Gerardo Cordero <cordero.gerard@gmail.com>
2019-02-07 00:07:01 -0500

    Create react app with typescript init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>No office location</h1>
+                    <h5>We will be back as soon as posible.</h5>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
