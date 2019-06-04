6d73f3023b0aade84e2a06b0d2f66d393635c2b1
Gerardo Cordero <cordero.gerard@gmail.com>
2019-06-03 23:30:01 -0500

    Setting SCSS for CSS Modules / Create LanguageSelector Component Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
-import './App.css';
+import appStyles from './App.module.scss';
 
 class App extends Component {
+    state = { language: 'english'};
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>No office location</h1>
-                    <h5>We will be back as soon as posible.</h5>
+            <div className={appStyles.app}>
+                <header className={appStyles.header}>
+                    <div className="ui container">
+                        <h1>No office location</h1>
+                        <h5>We will be back as soon as posible.</h5>
+                    </div>
                 </header>
             </div>
         );
