4a0c18ea0530cee3aed8138945c21e56cd52b9a2
Gerardo Cordero <cordero.gerard@gmail.com>
2019-06-22 13:37:05 -0500

    refactor on App to make it hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import appStyles from './App.module.scss';
 
-class App extends Component {
-    state = { language: 'english'};
-    render() {
-        return (
-            <div className={appStyles.app}>
-                <header className={appStyles.header}>
-                    <div className="ui container">
-                        <h1>No office location</h1>
-                        <h5>We will be back as soon as posible.</h5>
-                    </div>
-                </header>
-            </div>
+const App = () => {
+  const [data, updateData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json()
         );
-    }
-}
+      updateData(result);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className={appStyles.app}>
+      <header className={appStyles.header}>
+        <div className="ui container">
+          <h1>No office location</h1>
+          <h5>We will be back as soon as posible. </h5>
+        </div>
+      </header>
+      <section>
+        {data.map((info:any, index:number) => (<div key={info.id}>{info.title}</div>))}
+      </section>
+    </div>
+  );
+};
 
 export default App;
